085a3cf13f9d87766528c29fdd75cfe227d99610
dkaushik16 <133652665+dkaushik16@users.noreply.github.com>
2023-10-11 12:16:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.style.visibility === "visible") {
+      x.style.visibility = "hidden";
+    } else {
+      x.style.visibility = "visible";
+    }
+  }
